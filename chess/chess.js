e5241ebf5be927dab3de2a42ea89681a20eb719e
Alex <xandersdms@gmail.com>
2020-05-12 10:35:35 -0700

    Adding in Chess
    Adding in the code that will allow chess to be played.
    Modiffing the code in ssi/nav.html to show chess link.

---
+++ chess/chess.js
@@ -0,0 +1,53 @@
+const gameId = "#chess";
+
+let square;
+let player = "white";
+let cells  = document.querySelectorAll( gameId + " .board td" );
+
+window.addEventListener( "load", () => {
+    for ( let i = 0; i < cells.length; i++ ) {
+        cells[i].addEventListener( "click", turns );
+    }
+    document.querySelector( gameId + " button.reset" ).addEventListener( "click", reset );
+} );
+
+function turns(object) {
+    let target = ( object.target.parentNode.tagName == "TD" ) ? object.target.parentNode : object.target;
+
+    if ( target.firstChild && target.firstChild.classList.contains(player) ) {
+        if ( square ) {
+            square.classList.remove( "selected" );
+            for ( let i = 0; i < cells.length; i++ ) {
+                cells[i].removeEventListener( "click", turn );
+            }
+        }
+        square = target;
+        square.classList.add( "selected" );
+        for ( let i = 0; i < cells.length; i++ ) {
+            cells[i].addEventListener( "click", turn );
+        }
+    }
+}
+
+function turn(object) {
+    let place = ( object.target.parentNode.tagName == "TD" ) ? object.target.parentNode : object.target;
+    target    = document.querySelector( gameId + " .board .selected div");
+
+    if ( place.firstChild && place.firstChild.classList != null ) {
+        place.removeChild( place.childNodes[0] );
+        place.appendChild( target );
+    }
+    else {
+        place.appendChild( target );
+    }
+
+    player = ( player == "white" ) ? "black" : "white";
+
+    for ( let i = 0; i < cells.length; i++ ) {
+        cells[i].removeEventListener( "click", turn );
+    }
+}
+
+function reset() {
+    location.reload();
+}
